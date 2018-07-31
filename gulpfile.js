55dfa96ab703fa0cf1c3d65f6f1550f7002281a4
Huy Tran <hoang.huy.tran@gmail.com>
2018-07-31 17:39:21 +1000

    revises opengraph templates

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,25 @@ const cssnano = require("cssnano");
 const sourcemaps = require("gulp-sourcemaps");
 const htmlmin = require("gulp-htmlmin");
 const log = require("fancy-log");
+const del = require("del");
 
 const sassSourceFile = "themes/hyde-hyde/static-src/scss/hyde-hyde.scss";
 const watchedResources = "themes/hyde-hyde/static-src/scss/**/*";
 
 gulp.task("clean-project-css", function(done) {
-  let del = require("del");
   let targetFolder = "static/css";
   log.info("Cleaning relevant CSSs in '" + targetFolder + "'");
   del(targetFolder + "/hyde-hyde.*")
   done();
 });
 
+gulp.task("clean-dist", function(done) {
+    let targetFolder = "dist";
+    log.info("Cleaning '" + targetFolder + "'");
+    del([targetFolder + "/**", "!" + targetFolder, "!" + targetFolder + "/.*"]);
+    done();
+  });
+  
 gulp.task("compile-scss", function(done) {
   let themeOutputFolder = "themes/hyde-hyde/static/css";
   log("Compiling SCSSs to '" + themeOutputFolder + "'");
@@ -60,26 +67,23 @@ gulp.task("minify-scss", gulp.series("clean-project-css", function(done) {
     .on("end", done);
 }));
 
-gulp.task("minify-html", function(done) {
-  let publicFolder = "public";
-  let html = publicFolder + "/**/*.html";
+gulp.task("copy-public-to-dist", function(done) {
+    gulp
+        .src(["public/**", "!public/.*"])
+        .pipe(gulp.dest("dist")
+        .on("end", done));
+});
+  
+gulp.task("minify-resources", gulp.series(["clean-dist", "copy-public-to-dist"], function(done) {
+  let publicFolder = "./public";
+  let html = publicFolder + "/**/*ml";
   let css = publicFolder + "/**/*.css";
   let js = publicFolder + "/**/*.js";
   let dest = "dist";
   log("Minifying HTML/CSS/JS in '" + publicFolder + "' to '" + dest + "'");
-  gulp.src(publicFolder + "/**")
-    .pipe(gulp.dest(dest));
   gulp.src([html, css, js])
-    .pipe(htmlmin(
-        { 
-            collapseWhitespace: true,
-            removeComments: true
-        }
-    ))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest(dest))
     .on("end", done);
-});
+}));
 
-gulp.task("copy-public-to-dist", function(done) {
-  gulp.src("public/**").pipe(gulp.dest("dist").on("end", done));
-});
